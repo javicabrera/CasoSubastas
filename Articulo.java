5275f33ca12dfa14df67a855e5a3a07e4469a122
javicabrera <32722641+javicabrera@users.noreply.github.com>
2018-11-29 01:08:01 -0300

    Clonacion de clase articulo

---
+++ Articulo.java
@@ -0,0 +1,19 @@
+public class Articulo{
+	private persona Vendedor;
+	private pujas ArrayList<Integer>;
+	private descripcion String;
+	private nombre String;
+	private precio int;
+	private disponible boolean;
+
+	public Articulo(Vendedor v,String d,String n,String p){
+		persona=v;
+		pujas=new ArrayList();
+		descripcion=d;
+		nombre=n;
+		precio=p;
+		disponible=true;
+
+	}
+
+}
