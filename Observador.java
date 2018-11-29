8b088cda78e776845aa5bb2ffa3a4614803b3021
Unknown <jcabrera16@alumnos.utalca.cl>
2018-11-29 01:10:06 -0400

    Creacion clase observador que contiene a vendedor y comprador

---
+++ Observador.java
@@ -0,0 +1,11 @@
+public class Observador(){
+    private nombre String;
+    private vende boolean;
+
+    public Observador(String nombre, vende boolean){
+        this.nombre=nombre;
+        this.vende=vende;
+    }
+
+
+}
