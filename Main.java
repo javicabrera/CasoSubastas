a81b909dd120eb0a289da8bdfc8892f4f4056d77
Unknown <jcabrera16@alumnos.utalca.cl>
2018-11-29 00:26:00 -0400

    Creacion de clase principal

---
+++ Main.java
@@ -0,0 +1,10 @@
+public Class Main(){
+    private vendedores ArrayList<Vendedor>
+    private compradores ArrayList<Comprador>
+    private articulos ArrayList<Articulo>
+	
+	public static void main ( String[] args ){
+		
+	}
+
+}
