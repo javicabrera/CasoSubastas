234d3da26fa63aeaac01653eae32c6bbd2543391
Unknown <jcabrera16@alumnos.utalca.cl>
2018-11-29 01:17:43 -0400

    Creacion metodo registrar articulo

--- Observador.java
+++ Observador.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class Observador(){
     private nombre String;
     private vende boolean;
+    private Scanner sc = new Scanner(System.in);
 
     public Observador(String nombre, vende boolean){
         this.nombre=nombre;
         this.vende=vende;
     }
-
+    public Articulo registrar(Observador o){
+        String n=sc.nextLine();
+        String d=sc.nextLine();
+        int p=sc.nextInt();
+        Articulo a=new Articulo(o,d,n,p);
+        return a;
+    }
 
 }
