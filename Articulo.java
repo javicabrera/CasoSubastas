c3bbec5899b98ff218cf7958b74191bf9df55371
Unknown <jcabrera16@alumnos.utalca.cl>
2018-11-29 01:24:27 -0400

    Modificacion de algunos atributos de la clase articulo y nuevas funcionalidades

--- Articulo.java
+++ Articulo.java
@@ -1,12 +1,14 @@
+import java.util.ArrayList;
+
 public class Articulo{
-	private persona Vendedor;
+	private persona Observadorr;
 	private pujas ArrayList<Integer>;
 	private descripcion String;
 	private nombre String;
 	private precio int;
 	private disponible boolean;
 
-	public Articulo(Vendedor v,String d,String n,String p){
+	public Articulo(Observador v,String d,String n,String p){
 		persona=v;
 		pujas=new ArrayList();
 		descripcion=d;
@@ -14,6 +16,18 @@ public class Articulo{
 		precio=p;
 		disponible=true;
 
+	}
+
+	public void mostrarPujas(){
+		for(int i=0;i<pujas.size();i++){
+			System.out.println(pujas.get(i));
+		}	
+	}
+	public void modificar(){
+
+	}
+	public void asignar(Observador o){
+		this.persona=o;
 	}
 
 }
