8ed7e104e208c2cb8048019e9b3dae1b71b76505
Unknown <jcabrera16@alumnos.utalca.cl>
2018-11-29 01:35:09 -0400

    Modificaciones a clase principal
    nuevos metodos crear cuenta y mostrar lista de articulos

--- Main.java
+++ Main.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public Class Main(){
-    private vendedores ArrayList<Vendedor>
-    private compradores ArrayList<Comprador>
-    private articulos ArrayList<Articulo>
-	
+    private observadores ArrayList<Observadores>;
+    private articulos ArrayList<Articulo>;
+	private Scanner sc = new Scanner(System.in);
 	public static void main ( String[] args ){
 		
 	}
-
+    public void crearCuenta(){
+        Observador o;
+        String n=sc.nextLine();
+        System.out.println("Ingrese 0 si es vendedor o 1 si es comprador");
+        int x=nextInt();
+        if(x==0){
+            o=new Observador(n,true);
+        }
+        else{
+            o=new Observador(n,false);
+        }
+    }
+    public void mostrarArticulos(){
+        int i=0;
+        for(Articulo a: articulos){
+            System.out.println(i".- "+a.nombre);
+        }
+        int j=sc.nextInt();
+        for(int i=0;i<a.get(j).getPujas().size();i++{
+            System.out.println(a.get(j).getPujas().get(i));
+        }
+    }
 }
